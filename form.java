5ef3982b9f5a9144b2df35c1e030786e4c419af6
Ashish kumar <ashishchumber34@gmail.com>
2023-02-19 19:27:37 +0530

    initial commit

---
+++ form.java
@@ -0,0 +1,15 @@
+package letsuseit;
+
+/**
+ * form
+ */
+public class form {
+ public static void main(String[] args) {
+    
+     int a = 12 ;
+     int b = 34 ;
+     int sum = a + b;
+    System.out.println(sum);     
+    }
+
+}
